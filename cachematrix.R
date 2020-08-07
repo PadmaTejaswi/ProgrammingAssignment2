3ea635146e483a2864f84b01e8300958655c4076
PadmaTejaswi <68492466+PadmaTejaswi@users.noreply.github.com>
2020-08-07 18:52:38 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,16 +3,7 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
 makeCacheMatrix <- function(x = matrix()) {
   j<- NULL
   set<- function(y){
